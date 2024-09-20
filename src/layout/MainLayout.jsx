f0e18e4b40f3cedf2466d5ade7fcf8e38dda76b6
mdimranhossainwd <mdimranhossain7498@gmail.com>
2024-09-20 07:51:19 +0600

    devspotlight-package-created

---
+++ src/layout/MainLayout.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "../components/static/Navbar";
+
+const MainLayout = () => {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+};
+
+export default MainLayout;
