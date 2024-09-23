57efc125b009b1aa02fbc0c228d0f1ecd3f56417
mdimranhossainwd <mdimranhossain7498@gmail.com>
2024-09-23 09:23:01 +0600

    Firebase setup ADDED

---
+++ src/config/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBLPYydb5MwMyJTgAlxIZeyV4FPrWKkD2w",
+  authDomain: "devspotlight-c203a.firebaseapp.com",
+  projectId: "devspotlight-c203a",
+  storageBucket: "devspotlight-c203a.appspot.com",
+  messagingSenderId: "573268211590",
+  appId: "1:573268211590:web:b06aad47c1ec66319d1e07",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
