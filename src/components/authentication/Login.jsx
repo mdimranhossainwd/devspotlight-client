07901a418f5784bbe663a75fbb833e67041cc9e5
mdimranhossainwd <mdimranhossain7498@gmail.com>
2024-09-25 09:21:29 +0600

    Helmet Provider ADDED

--- src/components/authentication/Login.jsx
+++ src/components/authentication/Login.jsx
@@ -1,7 +1,7 @@
 import { useContext } from "react";
+import { Helmet } from "react-helmet-async";
 import { useForm } from "react-hook-form";
 import toast from "react-hot-toast";
-import { FaGoogle } from "react-icons/fa";
 import { Link, useNavigate } from "react-router-dom";
 import { AuthContext } from "../../provider/AuthProvider";
 
@@ -35,83 +35,93 @@ const Login = () => {
     }
   };
   return (
-    <div className="flex justify-center items-center min-h-screen bg-gray-100">
-      <div className="bg-white p-8 rounded-lg shadow-lg max-w-md w-full text-center">
-        <h2 className="text-4xl font-josefin font-bold text-gray-800 mb-3">
-          Welcome back
-        </h2>
-        <p className="text-gray-500 font-semibold mb-6">
-          Sign in to access your dashboard, settings, and projects.
-        </p>
-
-        <button
-          onClick={handleGoogleSignUp}
-          className="w-full flex items-center justify-center gap-3 text-md border-gray-300 border text-black font-semibold py-3 rounded-md mb-6"
-        >
-          <FaGoogle />
-          <span>Connect with Google</span>
-        </button>
-
-        <div className="relative text-gray-400 mb-6">
-          <span className="absolute inset-x-0 top-1/2 transform -translate-y-1/2 bg-white px-2 text-sm">
-            or sign in with email
-          </span>
-          <div className="border-t border-gray-300"></div>
-        </div>
-
-        <form onSubmit={handleSubmit(handleLogin)}>
-          <div className="mb-4 text-left">
-            <label
-              htmlFor="email"
-              className="block text-gray-600 text-md font-semibold mb-1"
-            >
-              Email
-            </label>
-            <input
-              type="email"
-              id="email"
-              {...register("email", { required: true })}
-              className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-orange-500"
-              placeholder="Enter your email"
-            />
-          </div>
-
-          <div className="mb-4 text-left">
-            <label
-              htmlFor="password"
-              className="block text-gray-600 text-md font-semibold mb-1"
-            >
-              Password
-            </label>
-            <input
-              type="password"
-              id="password"
-              {...register("password", { required: true })}
-              className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-orange-500"
-              placeholder="Enter your password"
-            />
-          </div>
-
-          <div className="flex items-center mb-6">
-            <input type="checkbox" id="remember" className="mr-2" />
-            <label htmlFor="remember" className="text-gray-600 text-sm">
-              Remember for 30 days
-            </label>
-          </div>
+    <div>
+      <Helmet>
+        <title>devspotlight || Login Page</title>
+      </Helmet>
+      <div className="flex justify-center items-center min-h-screen bg-gray-100">
+        <div className="bg-white p-8 rounded-lg shadow-lg max-w-md w-full text-center">
+          <h2 className="text-4xl font-josefin font-bold text-gray-800 mb-3">
+            Welcome back
+          </h2>
+          <p className="text-gray-500 font-semibold mb-6">
+            Sign in to access your dashboard, settings, and projects.
+          </p>
 
           <button
-            type="submit"
-            className="w-full bg-gradient-to-r from-[#7ed56f] to-[#28b485] text-white py-3 rounded-md "
+            onClick={handleGoogleSignUp}
+            className="w-full flex items-center justify-center gap-3 text-md border-gray-300 border text-black font-semibold py-3 rounded-md mb-6"
           >
-            Sign in
+            {/* <FaGoogle /> */}
+            <img
+              className="w-5 h-5"
+              src="https://i.ibb.co.com/xfm5X7r/Google-G-logo-svg-removebg-preview.png"
+              alt=""
+            />
+            <span>Sign In with Google</span>
           </button>
-        </form>
 
-        <div className="mt-6 text-md font-semibold text-gray-500">
-          No account?{" "}
-          <Link to="/signup" className="text-black">
-            Create an account
-          </Link>
+          <div className="relative text-gray-400 mb-6">
+            <span className="absolute inset-x-0 top-1/2 transform -translate-y-1/2 bg-white px-2 text-sm">
+              or sign in with email
+            </span>
+            <div className="border-t border-gray-300"></div>
+          </div>
+
+          <form onSubmit={handleSubmit(handleLogin)}>
+            <div className="mb-4 text-left">
+              <label
+                htmlFor="email"
+                className="block text-gray-600 text-md font-semibold mb-1"
+              >
+                Email
+              </label>
+              <input
+                type="email"
+                id="email"
+                {...register("email", { required: true })}
+                className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-orange-500"
+                placeholder="Enter your email"
+              />
+            </div>
+
+            <div className="mb-4 text-left">
+              <label
+                htmlFor="password"
+                className="block text-gray-600 text-md font-semibold mb-1"
+              >
+                Password
+              </label>
+              <input
+                type="password"
+                id="password"
+                {...register("password", { required: true })}
+                className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-orange-500"
+                placeholder="Enter your password"
+              />
+            </div>
+
+            <div className="flex items-center mb-6">
+              <input type="checkbox" id="remember" className="mr-2" />
+              <label htmlFor="remember" className="text-gray-600 text-sm">
+                Remember for 30 days
+              </label>
+            </div>
+
+            <button
+              type="submit"
+              className="w-full bg-gradient-to-r from-[#7ed56f] to-[#28b485] text-white py-3 rounded-md "
+            >
+              Sign in
+            </button>
+          </form>
+
+          <div className="mt-6 text-md font-semibold text-gray-500">
+            No account?{" "}
+            <Link to="/signup" className="text-black">
+              Create an account
+            </Link>
+          </div>
         </div>
       </div>
     </div>
--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { HelmetProvider } from "react-helmet-async";
 import { Toaster } from "react-hot-toast";
 import { RouterProvider } from "react-router-dom";
 import "./index.css";
@@ -10,7 +11,9 @@ createRoot(document.getElementById("root")).render(
   <StrictMode>
     <AuthProvider>
       <Toaster />
-      <RouterProvider router={router} />
+      <HelmetProvider>
+        <RouterProvider router={router} />
+      </HelmetProvider>
     </AuthProvider>
   </StrictMode>
 );
