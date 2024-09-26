2598e32c1bdf6cd708decb54b4baaa62e7d9e219
mdimranhossainwd <mdimranhossain7498@gmail.com>
2024-09-26 17:08:03 +0600

    trending sections added in UI

--- src/pages/HomePages.jsx
+++ src/pages/HomePages.jsx
@@ -2,11 +2,13 @@ import Carousel from "../components/Carousel";
 import Brand from "../components/extra/Brand";
 import Newsletter from "../components/extra/Newsletter";
 import FeatureSection from "../components/FeatureSection";
+import TreadSections from "../components/TreadSections";
 const HomePages = () => {
   return (
     <>
       <Carousel />
       <FeatureSection />
+      <TreadSections />
       <Newsletter />
       <Brand />
     </>
