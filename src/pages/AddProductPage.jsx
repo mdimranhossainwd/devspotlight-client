cb69071e663d7f76440f60cb388f57b0eccc4436
mdimranhossainwd <mdimranhossain7498@gmail.com>
2024-10-01 16:58:30 +0600

    feat: Input Tags Sections package Install

--- src/pages/AddProductPage.jsx
+++ src/pages/AddProductPage.jsx
@@ -1,7 +1,11 @@
+import { useState } from "react";
+import { TagsInput } from "react-tag-input-component";
 import useAuth from "../hooks/useAuth";
+
 const AddProductPage = () => {
   const { user } = useAuth();
   console.log(user);
+  const [selected, setSelected] = useState([]);
 
   return (
     <div className="max-w-3xl mx-auto my-6 p-6 bg-white shadow-md rounded-lg">
@@ -76,14 +80,15 @@ const AddProductPage = () => {
 
         {/* Tags */}
         <div>
-          <label className="block text-sm font-medium text-gray-700">
+          <label className="block text-sm mb-1 font-medium text-gray-700">
             Tags
           </label>
-          <input
-            type="text"
-            placeholder="Product Tags"
+
+          <TagsInput
+            value={selected}
+            onChange={setSelected}
             name="tags"
-            required
+            placeHolder="Enter Tags"
             className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-[#7ed56f] focus:border-[#7ed56f] sm:text-sm"
           />
         </div>
